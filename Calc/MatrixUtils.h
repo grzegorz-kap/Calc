ae0d54739079182f7254d42d0673bb9a1b5fff93
grzk695 <grzk@outlook.com>
2014-11-17 21:28:05 +0100

    MatrixUtils refactor

--- Calc/MatrixUtils.h
+++ Calc/MatrixUtils.h
@@ -16,23 +16,221 @@ namespace PR
 	public:
 
 		template<class T>
-		static int lu(const Matrix<T> &src, Matrix<T> **L, Matrix<T> **U = nullptr, Matrix<T> **P=nullptr);
+		static int lu(const Matrix<T> &a, Matrix<T> **l, Matrix<T> **u=nullptr, Matrix<T> **p=nullptr)
+		{
+			if (a.M != a.N)
+				NumericException::throwLuNotSquare();
+
+			vector<int> s;
+			vector<int> d;
+
+			int swaps_count = 0;
+
+			/*Doolittle-Crout*/
+			std::unique_ptr<Matrix<T>> i_l = std::make_unique<Matrix<T>>(a);
+			std::unique_ptr<Matrix<T>> i_p;
+			Matrix<T> &A = *i_l;
+			int m = A.M;
+			int n = A.N;
+
+			if (p)
+			{
+				if (*p == nullptr)
+					i_p = make_unique<Matrix<T>>(MatrixBuilder<T>::buildEye(a.M, a.N));
+				else
+					i_p.reset(*p);
+				if (i_p->rows() != a.M)
+					throw CalcException("LU decompositor permutation matrix has wrong rows count");
+			}
+
+			for (int k = 0; k < m - 1; k++)
+			{
+				/*Finding max module*/
+				T max = 0;
+				int idx = k;
+				for (int i = k; i < m; i++)
+				{
+					T module = Mathematic::module(A.mx[i][k]);
+					if (module>max)
+					{
+						max = module;
+						idx = i;
+					}
+				}
+
+				/* Swap vectors if neccessary */
+				if (k != idx)
+				{
+					std::swap(A.mx[k], A.mx[idx]);
+					++swaps_count;
+					if (p)
+						std::swap(i_p->mx[k], i_p->mx[idx]);
+					else if (u)
+					{
+						s.push_back(k);
+						d.push_back(idx);
+					}
+				}
+
+				ComplexNumber<T> &ref = A.mx[k][k];
+				for (int j = k + 1; j < n; j++)
+					A.mx[j][k] /= ref;
+
+				for (int i = k + 1; i < m; i++)
+					for (int j = k + 1; j < n; j++)
+						if (i != k)
+							A.mx[i][j] -= A.mx[i][k] * A.mx[k][j];
+			}
+			Matrix<T> *n_u = nullptr;
+			if (u)
+			{
+				n_u = new Matrix<T>(m, n);
+				for (int i = 0; i < m; i++)
+					for (int j = 0; j < n; j++)
+					{
+						if (i == j){ n_u->mx[i][j] = std::move(A.mx[i][j]); A.mx[i][j] = ComplexNumber<T>(1); }
+						else if (i < j){ n_u->mx[i][j] = std::move(A.mx[i][j]); A.mx[i][j] = ComplexNumber<T>(0); }
+						else { n_u->mx[i][j] = ComplexNumber<T>(0); }
+					}
+			}
+
+			if (u&&!p)
+			{
+				for (int i = s.size() - 1; i >= 0; i--)
+					std::swap(A.mx[s[i]], A.mx[d[i]]);
+			}
+
+			*l = i_l.release();
+			if (p)
+				*p = i_p.release();
+			if (u)
+				*u = n_u;
+			return swaps_count;
+		}
 
 		template <class T>
-		static int lu(const ComplexNumber<T> &src, ComplexNumber<T> **L, ComplexNumber<T> **U = nullptr, ComplexNumber<T> **P = nullptr);
+		static int lu(const ComplexNumber<T> &src, ComplexNumber<T> **L, ComplexNumber<T> **U = nullptr, ComplexNumber<T> **P = nullptr)
+		{
+			*L = new ComplexNumber<T>(src);
+			if (U) *U = new ComplexNumber<T>(src);
+			if (P) *P = new ComplexNumber<T>(1);
+			return 0;
+		}
 
-		template <class T> static ComplexNumber<T> det(const Matrix<T> &a);
-		template <class T> static ComplexNumber<T> det(const ComplexNumber<T> &a);
-		template <class T> static ComplexNumber<T> inv(const ComplexNumber<T> &a);
-		template <class T> static Matrix<T> inv(const Matrix<T> &A);
+		template <class T> static ComplexNumber<T> det(const Matrix<T> &a)
+		{
+			if (a.M != a.N)
+				throw NumericException("Cannot compute determinant from not squere matrix");
+			if (a.M == 0)
+				return ComplexNumber<T>(1);
 
-		template <class T> static Matrix<T> divide(const Matrix<T> &AA, const Matrix<T> &BB);
-		template <class T> static void url(const Matrix<T> &A, const Matrix<T> &b, int j_b, Matrix<T> &x, int j_x);
+			Matrix<T> *ptr = nullptr;
+			bool sign = lu(a, &ptr) % 2 ? true : false;
+			auto &mx = ptr->mx;
+			ComplexNumber<T> det = mx[0][0];
+			for (int i = 1; i < a.M; i++)
+				det *= mx[i][i];
+			SafeRealase(&ptr);
+			if (sign)
+				return det.neg();
+			else
+				return det;
+		}
 
-		template <class T> static Matrix<T> createVector(const Matrix<T> &start, const Matrix<T> &end);
-		template <class T> static Matrix<T> createVector(const ComplexNumber<T> &start, const ComplexNumber<T> &end);
-		template <class T> static Matrix<T> createVector(const Matrix<T> &start, const Matrix<T> &step, const Matrix<T> &end);
-		template <class T> static Matrix<T> createVector(const ComplexNumber<T> &start, const ComplexNumber<T> &step, const ComplexNumber<T> &end);
+		template <class T> static ComplexNumber<T> det(const ComplexNumber<T> &a)
+		{
+			return a;
+		}
+
+		template <class T> static ComplexNumber<T> inv(const ComplexNumber<T> &a)
+		{
+			return ComplexNumber<T>(1) / a;
+		}
+		
+		template <class T> static Matrix<T> inv(const Matrix<T> &A)
+		{
+			Matrix<T> *lu = nullptr;
+			Matrix<T> *eye = nullptr;
+
+			MatrixUtils::lu(A, &lu, (Matrix<T>**)nullptr, &eye);
+			Matrix<T> &x = *eye;
+			Matrix<T> &a = *lu;
+
+			unique_ptr<Matrix<T>> x_deleter(eye);
+
+			for (int j = 0; j < x.N; j++)
+			{
+				url(a, x, j, x, j);
+			}
+			SafeRealase(&lu);
+			return x;
+		}
+
+		template <class T> static Matrix<T> divide(const Matrix<T> &AA, const Matrix<T> &BB)
+		{
+			return AA*inv(BB);
+		}
+
+		template <class T> static void url(const Matrix<T> &A, const Matrix<T> &b, int j_b, Matrix<T> &x, int j_x)
+		{
+			x.mx[0][j_x] = b.mx[0][j_b];
+			for (int i = 1; i < A.M; i++)
+			{
+				ComplexNumber<T> sum(0);
+				for (int j = 0; j < i; j++)
+					sum += A.mx[i][j] * x.mx[j][j_x];
+				x.mx[i][j_x] = b.mx[i][j_b] - sum;
+			}
+
+			x.mx.back()[j_x] /= A.mx.back().back();
+			for (int i = A.M - 2; i >= 0; i--)
+			{
+				ComplexNumber<T> sum(0);
+				for (int j = i + 1; j < A.M; j++)
+					sum += A.mx[i][j] * x.mx[j][j_x];
+				x.mx[i][j_x] -= sum;
+				x.mx[i][j_x] /= A.mx[i][i];
+			}
+		}
+
+		template <class T> static Matrix<T> createVector(const Matrix<T> &start, const Matrix<T> &end)
+		{
+			if (start.isEmpty() || end.isEmpty())
+				return Matrix<T>(1, 0);
+
+			return createVector(start.mx[0][0], end.mx[0][0]);
+		}
+
+
+		template <class T> static Matrix<T> createVector(const ComplexNumber<T> &start, const ComplexNumber<T> &end)
+		{
+			return createVector(start, ComplexNumber<T>(1), end);
+		}
+
+		template <class T> static Matrix<T> createVector(const Matrix<T> &start, const Matrix<T> &step, const Matrix<T> &end)
+		{
+			if (start.isEmpty() || end.isEmpty() || step.isEmpty())
+				return Matrix<T>(1, 0);
+			return createVector(start.mx[0][0], step.mx[0][0], end.mx[0][0]);
+		}
+
+		template <class T> static Matrix<T> createVector(const ComplexNumber<T> &start, const ComplexNumber<T> &step, const ComplexNumber<T> &end)
+		{
+			int m = (int)Mathematic::fix((end - start) / step).getRe();
+
+			if (start.getIm() != 0 || step.getIm() != 0 || end.getIm() != 0 || m<0)
+				return Matrix<T>(1, 0);
+
+			const T &j = start.getRe();
+			const T &i = step.getRe();
+			Matrix<T> out(1, m + 1);
+			auto &ref = out.mx[0];
+
+			for (int k = 0; k <= m; k++)
+				ref[k] = j + k*i;
+
+			return out;;
+		}
 	};
 
 }
