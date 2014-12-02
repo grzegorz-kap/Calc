7e3e691bbbacc1bd5387bb6cbf11c45db59951e4
grzk695 <grzk@outlook.com>
2014-12-02 15:06:35 +0100

    Trigonometry functions in single header and cpp file

---
+++ Calc/TrigonometryFunctions.cpp
@@ -0,0 +1,52 @@
+#include "stdafx.h"
+#include "TrigonometryFunctions.h"
+
+namespace PR
+{
+	CosFun::CosFun()
+	{
+		min_args_count = max_args_count = 1;
+		name = "sin";
+	}
+
+	CosFun::~CosFun()
+	{
+	}
+
+	shared_ptr<Data> CosFun::run()
+	{
+		return arguments[0]->cos();
+	}
+
+	SinFun::SinFun()
+	{
+		min_args_count = max_args_count = 1;
+		name = "sin";
+	}
+
+
+	SinFun::~SinFun()
+	{
+	}
+
+	shared_ptr<Data> SinFun::run()
+	{
+		return arguments[0]->sin();
+	}
+
+	TanFun::TanFun()
+	{
+		max_args_count = min_args_count = 1;
+		name = "tan";
+	}
+
+
+	TanFun::~TanFun()
+	{
+	}
+
+	shared_ptr<Data> TanFun::run()
+	{
+		return arguments[0]->tan();
+	}
+}
