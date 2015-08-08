66838664e79f9ba7a1e03d8cfb63642e392fb490
Grzegorz Kapica <grzk@outlook.com>
2015-08-08 13:37:55 +0200

    Setup BoostUnitTest project

---
+++ CalcTest/main.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE klab
+#include <boost/test/included/unit_test.hpp>
+#include <iostream>
+
+#include "RegexTokenizerTest.h"
+
+using namespace std;
+
+BOOST_AUTO_TEST_CASE(testing_test) {
+	system("pause");
+}
